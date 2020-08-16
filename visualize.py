0524f44563eb4514069122b7e7177b3957cb749a
Martin <marstin@umich.edu>
2020-08-15 21:32:20 -0400

    clear

--- visualize.py
+++ visualize.py
@@ -3,44 +3,14 @@ from matplotlib_venn import venn2
 import pandas as pd
 import numpy as np
 
-
-def get_size(ch):
-    sizes = {
-        'chr1': 248956422,
-        'chr2': 242193529,
-        'chr3': 198295559,
-        'chr4': 190214555,
-        'chr5': 181538259,
-        'chr6': 170805979,
-        'chr7': 159345973,
-        'chr8': 145138636,
-        'chr9': 138394717,
-        'chr10': 133797422,
-        'chr11': 135086622,
-        'chr12': 133275309,
-        'chr13': 114364328,
-        'chr14': 107043718,
-        'chr15': 101991189,
-        'chr16': 90338345,
-        'chr17': 83257441,
-        'chr18': 80373285,
-        'chr19': 58617616,
-        'chr20': 64444167,
-        'chr21': 46709983,
-        'chr22': 50818468,
-        'chrX': 156040895,
-        'chrY': 57227415
-    }
-    if ch == 'all':
-        return sum(sizes.values())
-    return sizes[ch]
+from utils import load_chrom_sizes
 
 
 def has_intersection(domain1, domain2):
     return domain1[0] <= domain2[1] and domain1[1] >= domain2[0]
 
 
-def comp(cell1, cell2, tolerance=10000):
+def comp(cell1, cell2, reference_genome='hg38', tolerance=10000):
     cell1_file = "stripes/{}.signal".format(cell1)
     cell2_file = "stripes/{}.signal".format(cell2)
 
@@ -48,11 +18,7 @@ def comp(cell1, cell2, tolerance=10000):
     unique_cell1 = []
     shared = []
 
-    chromes = ['chr1', 'chr2', 'chr3', 'chr4', 'chr5',
-               'chr6', 'chr7', 'chr8', 'chr9', 'chr10',
-               'chr11', 'chr12', 'chr13', 'chr14', 'chr15',
-               'chr16', 'chr17', 'chr18', 'chr19', 'chr20',
-               'chr21', 'chr22', 'chrX', 'chrY']
+    chromes = load_chrom_sizes(reference_genome).keys()
 
     df_cell1 = pd.read_csv(cell1_file, sep='\t')
     df_cell2 = pd.read_csv(cell2_file, sep='\t')
